a660c1ef90d7c1d1f68550535693afabf7df2e70
tipubandlapalli <bstippusultan@gmail.com>
2025-04-06 16:47:14 +0530

    userDetailsService

--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,29 +27,20 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
-
-
-
-
-    /*
-    // now it will not need userName & password
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(customizer -> customizer.disable()); // csrf disabled
-        // now not even csrf-needed for post put delete request
-        // we our config now
-        httpSecurity.authorizeHttpRequests(req -> req.anyRequest().authenticated());
-        //for browser to display default login form
-        // httpSecurity.formLogin(Customizer.withDefaults());
-        // for post man to take user and password from auth basic
-        httpSecurity.httpBasic(Customizer.withDefaults());
-        // making session stateless; everytime the user send a request new session is created means needs authorization
-        // as we enabled browser to send login form it will throw login form continues
-        // for this not to happen comment it
-        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        return httpSecurity.build();
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("aram")
+                .password("aram")
+                .roles("USER")
+                .build();
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("tipu")
+                .password("tipu")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1,user2);
     }
-
-     */
 }
